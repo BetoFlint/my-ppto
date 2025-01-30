fdaec6bebe5639379d6b03e0fdaf9e0ce4b48c4d
BetoFlint <jalbornozr2@gmail.com>
2025-01-29 22:58:36 -0300

    Cambios configuración

--- db/connection.ts
+++ db/connection.ts
@@ -1,30 +1,14 @@
-/*import { Client } from "postgres";
-
-const client = new Client({
-  user: "usuarioppto",
-  database: "bdppto",
-  hostname: "localhost",
-  port: 5432,
-  password: "ppto1234",
-});
-
-export async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Database connected successfully!");
-  } catch (err) {
-    console.error("Connection error:", err);
-  }
-  return client;
-}
-*/
-
-// db/connection.ts
 import { Client } from "postgres";
 import { load } from "https://deno.land/std/dotenv/mod.ts";
 
 const env = await load();
 const databaseUrl = env["DATABASE_URL"] || Deno.env.get("DATABASE_URL");
+
+if (!databaseUrl) {
+  throw new Error(
+    "DATABASE_URL is not defined in the .env file or environment variables."
+  );
+}
 
 const client = new Client(databaseUrl);
 
@@ -37,3 +21,5 @@ export async function connectDB() {
   }
   return client;
 }
+
+// Puedes exportar otras funciones también si es necesario
